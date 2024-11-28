dadf0671a73a380442c38f48d75307115c406aaf
myfirstdebianpc <rurayi@yahoo.fr>
2024-11-28 23:01:20 +0100

    simple-bot, js mini bot for bsky

---
+++ src/main.ts
@@ -0,0 +1,54 @@
+import { BskyAgent, AppBskyGraphDefs } from '@atproto/api';
+import * as dotenv from 'dotenv';
+import { CronJob } from 'cron';
+import * as process from 'process';
+
+dotenv.config();
+
+// Create a Bluesky Agent 
+const agent = new BskyAgent({
+    service: 'https://bsky.social',
+  });
+
+//static getAbablipFollowers {
+async function getAbablipFollowers() {
+  //const { uri } = 'at://did:plc:plveesmuzs3rizet3tfc5b56/app.bsky.graph.list/3lbpju5w4il25'
+  let uri = 'at://did:plc:plveesmuzs3rizet3tfc5b56/app.bsky.graph.list/3lbpju5w4il25';
+  let members: AppBskyGraphDefs.ListItemView[] = [];
+  let cursor: string | undefined;
+  do {
+    let res = await agent.app.bsky.graph.getList({
+      list: uri,
+      limit: 30,
+      cursor
+    });
+    cursor = res.data.cursor;
+    members = members.concat(res.data.items);
+  } while (cursor)
+  return members;
+}
+
+async function main() {
+  await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD!});
+  // let abablips = getAbablipFollowers();
+  let abatest = getAbablipFollowers();
+  //await agent.post({
+  //    text: "🙂"
+  //});
+  console.log("Just posted!");
+  //console.log("show abablip!", abablips);
+  console.log("show abablip!", abatest);
+}
+
+main();
+
+
+// Run this on a cron job
+const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
+//const scheduleExpressionFifteenMinute = '*/15 * * * *'; // Run once every 15 minutes
+//const scheduleExpression = '0 */3 * * *'; // Run once every three hours in prod
+
+const job = new CronJob(scheduleExpressionMinute, main); // change to scheduleExpressionMinute for testing
+//const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
+
+job.start();
